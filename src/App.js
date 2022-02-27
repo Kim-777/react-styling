d9be71a03e85a3ea0b66e73e57963c0761781177
Kim-777 <iloo3012@naver.com>
2022-02-27 22:30:20 +0900

    test styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import styled from "styled-components";
+import Test from "./components/Test";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.wrapperColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const TestWrapper = styled(Test)`
+  background-color: yellow;
+`;
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <Father style={{ display: "flex" }}>
+      <Box wrapperColor="red">
+        <Text>test</Text>
+      </Box>
+      <Box wrapperColor="teal" />
+      <TestWrapper />
+    </Father>
+  );
 }
 
 export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = (props) => {
+  return <div {...props}> Test</div>;
+};
+
+export default Test;
